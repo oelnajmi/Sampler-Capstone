acbdc3feb454c7f4d45f60cee991c39b4fdd34a3
Yuki Cao <jcao104@uottawa.ca>
2019-10-01 14:51:44 -0400

    Add listen page

--- components/pages/listen.py
+++ components/pages/listen.py
@@ -1,12 +1,16 @@
 from components.pages.page import Page
 import tkinter as tk
 from components.home_button import HomeButton
+from services.files_services import FilesServices
+from pydub.playback import play
 
 
 class Listen(Page):
     def __init__(self, global_app):
         Page.__init__(self)
         HomeButton(self, global_app)
-        label = tk.Label(self, text="Listen")
+        label = tk.Label(self, FilesServices.get_tracks())
+        sound = FilesServices.get_tracks()
+        play(sound)
         label.pack(side="top", fill="both", expand=True)
 
