19ef7de5aa0055be50e9045d12613b55ac672e54
Yuki Cao <jcao104@uottawa.ca>
2019-10-01 23:03:36 -0400

    play

--- components/pages/listen.py
+++ components/pages/listen.py
@@ -3,13 +3,10 @@ import tkinter as tk
 from components.home_button import HomeButton
 from services.files_services import FilesServices
 
-
-
 class Listen(Page):
     def __init__(self, global_app):
         Page.__init__(self)
         HomeButton(self, global_app)
-
         for song in FilesServices.get_tracks():
             label = tk.Label(self, text=song)
             label.pack(side="top", fill="both", expand=True)
