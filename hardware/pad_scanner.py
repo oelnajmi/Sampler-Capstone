70d46aba98b085346995dc534dad6b5ec2d56eb3
SamuelBeshara <samuelbeshara@hotmail.ca>
2019-11-05 13:08:28 -0500

    Allow code to run on non rapsberry pi devices

--- hardware/pad_scanner.py
+++ hardware/pad_scanner.py
@@ -1,10 +1,17 @@
 from threading import Thread
-import RPi.GPIO as GPIO
+
+###########################################################################
+## To allow this to compile on our computers, the raspberry pi code has been
+## commented. When running on a raspberry pi, uncomment all commented code
+## and remove all `True` lines for each loop below
+###########################################################################
+
+#import RPi.GPIO as GPIO
 import time
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setwarnings(False)
     
 COL = [5,11,9,10]
 ROW = [26,19,13,6]
@@ -19,23 +26,26 @@ class PadScanner(Thread):
 
     def run(self):
         for i in range(4):
-            GPIO.setup(ROW[i], GPIO.OUT)
-            GPIO.output(ROW[i], 0)
+            True
+            #GPIO.setup(ROW[i], GPIO.OUT)
+            #GPIO.output(ROW[i], 0)
 
         for j in range(4):
-            GPIO.setup(COL[j], GPIO.IN)
+            True
+            #GPIO.setup(COL[j], GPIO.IN)
                 
         while True:
-            for i in range(4):                
-                GPIO.output(ROW[i],1)
-                for j in range(4):
-                    if GPIO.input(COL[j]) == 1:
-                        print("col = " + str((j+1)))
-                        print("row = " + str((i+1)))
-                        print(" ")
-                        self.interface.click_pad(i, j)
-                        time.sleep(0.5)
-                    while(GPIO.input(COL[j]) == 1):
-                        time.sleep (0.2)
-                GPIO.output(ROW[i],0)
+            True
+            # for i in range(4):
+            #     GPIO.output(ROW[i],1)
+            #     for j in range(4):
+            #         if GPIO.input(COL[j]) == 1:
+            #             print("col = " + str((j+1)))
+            #             print("row = " + str((i+1)))
+            #             print(" ")
+            #             self.interface.click_pad(i, j)
+            #             time.sleep(0.5)
+            #         while(GPIO.input(COL[j]) == 1):
+            #             time.sleep (0.2)
+            #     GPIO.output(ROW[i],0)
 
